bf8d8eaf4fe7398c67e03a02f02b177693c87e0b
JoatDelia <Jesse_Hammond@cox.net>
2017-10-13 16:34:59 -0500

    Basic Box
    Added the Box class. Yes, I'm thinking inside the box. Later subclasses planned for the box are NarrativeBox and SelectBox

---
+++ boxes.py
@@ -0,0 +1,44 @@
+import libtcodpy as rl
+
+class Box: # The class for completely non-interactive boxes, superclass for interactive boxes.
+    def __init__(self,x,y,w,h,title=None,text=""):
+        # Box coordinates.
+        self.x = x
+        self.y = y
+        # Box dimensions.
+        self.w = w
+        self.h = h
+        self.title = title # To show at the top of the box.
+        self.text = text # To show in the box itself. It's up to the designer to ensure this does not exceed the bounds of the box. For story text boxes that may exceed the initial box, see NarrativeBox.
+    
+    def draw(self,color=rl.white):
+        oldColor = rl.console_get_default_foreground(0) # Store old color.
+        rl.console_set_default_foreground(0, color) # Apply window's color.
+        # Draw the corners.
+        rl.console_set_char(0, self.x, self.y, rl.CHAR_DNW)
+        rl.console_set_char(0, self.x+self.w-1, self.y, rl.CHAR_DNE)
+        rl.console_set_char(0, self.x, self.y+self.h-1, rl.CHAR_DSW)
+        rl.console_set_char(0, self.x+self.w-1, self.y+self.h-1, rl.CHAR_DSE)
+        # Draw the walls.
+        for i in range(self.y+1,self.y+self.h-1):
+            rl.console_set_char(0, self.x, i, rl.CHAR_DVLINE)
+            rl.console_set_char(0, self.x+self.w-1, i, rl.CHAR_DVLINE)
+        for i in range(self.x+1,self.x+self.w-1):
+            rl.console_set_char(0, i, self.y, rl.CHAR_DHLINE)
+            rl.console_set_char(0, i, self.y+self.h-1, rl.CHAR_DHLINE)
+        if self.title != None: # Draw the title, if present.
+            rl.console_print(0,self.x+2,self.y," {0} ".format(self.title))
+        rl.console_set_default_foreground(0, oldColor) # Revert color before drawing rest of window.
+        rl.console_print_rect(0, self.x+2, self.y+1, self.w-4, self.h-2, self.text) # Draw the inner text.
+    
+    def forward(self): # If enter or the like is pressed and this is the active box.
+        return True # Close the box.
+    
+    def backward(self): # If escape or the like is pressed and this is the active box.
+        return True # Close the box.
+    
+    def goUp(self): # If up or the like is pressed.
+        return # Do nothing. This isn't a selection box.
+    
+    def goDown(self): # If down or the like is pressed.
+        return # Do nothing. This isn't a selection box.
--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import libtcodpy as rl
 
+import boxes as bx
+
 def main():
     rl.console_set_custom_font('terminal8x14_gs_ro.png'.encode(), rl.FONT_TYPE_GREYSCALE | rl.FONT_LAYOUT_ASCII_INROW) # Sets the default font to something less square, making the resulting window look less flat. #8x14 normally
     rl.console_init_root(80, 24, 'Nafinian Nexus'.encode()) # Opens the pseudo-console window.
@@ -8,6 +10,8 @@ def main():
     while not rl.console_is_window_closed(): # As long as the X icon is not pressed...
         rl.console_clear(0) # Fill the window the background color.
         rl.console_print(0, 7, 2, "#  #  ##  #### ###  #  # ###  ##  #  #     #  # #### #  # #  #  ###\n## # #  # #     #   ## #  #  #  # ## #     ## # #    #  # #  # #\n# ## #### ###   #   # ##  #  #### # ##     # ## ###   ##  #  #  ##\n#  # #  # #     #   #  #  #  #  # #  #     #  # #    #  # #  #    #\n#  # #  # #    ###  #  # ### #  # #  #     #  # #### #  #  ##  ###") # Draw the title.
+        testBox = bx.Box(33,17,14,5,None,'{0} New Game\n  Continue\n  Modding'.format(chr(rl.CHAR_ARROW2_E)).encode()) # A test box. Will, of course, be replaced later with something more interactive.
+        testBox.draw() # Draws the test box on the screen.
         rl.console_flush() # Update the console (if this isn't done, the console will always just show a blank white screen).
         rl.console_wait_for_keypress(True) # Halt until a key is pressed. Do nothing withthe key press in this case.
     
