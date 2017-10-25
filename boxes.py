b45fcb348620abf6df34d1f3dbcb9bbfa4fd2f25
JoatDelia <Jesse_Hammond@cox.net>
2017-10-25 14:59:21 -0500

    Scrolling SelectBox
    SelectBox is now capable of allowing the user to scroll through more options than the box has room for. This should be useful for really knowledgable spellcasters whose spell list is too mighty for the height of the screen to contain.

--- boxes.py
+++ boxes.py
@@ -1,5 +1,7 @@
 import libtcodpy as rl
 
+import time
+
 class Box: # The class for completely non-interactive boxes, superclass for interactive boxes.
     def __init__(self,x,y,w,h,title=None,text=""):
         # Box coordinates.
@@ -44,7 +46,7 @@ class Box: # The class for completely non-interactive boxes, superclass for inte
         return # Do nothing. This isn't a selection box.
 
 class SelectBox(Box): # This box allows selecting from a number of options.
-    def __init__(self,x,y,w,h,title,options):
+    def __init__(self,x,y,w,h,title,options,optCap):
         # Box coordinates.
         self.x = x
         self.y = y
@@ -54,6 +56,8 @@ class SelectBox(Box): # This box allows selecting from a number of options.
         self.title = title # To show at the top of the box.
         self.options = options # To show in the box itself. It's up to the designer to ensure this does not exceed the bounds of the box. For story text boxes that may exceed the initial box, see NarrativeBox.
         self.selectedOption = 0 # The number of the currently selected option.
+        self.optCap = optCap # The maximum number of options to show at once.
+        self.optOffset = 0 # Controls which option should be the top option. This is needed for one to scroll through more options than the box has room for.
     
     def draw(self,color=rl.white):
         oldColor = rl.console_get_default_foreground(0) # Store old color.
@@ -73,9 +77,15 @@ class SelectBox(Box): # This box allows selecting from a number of options.
         if self.title != None: # Draw the title, if present.
             rl.console_print(0,self.x+2,self.y," {0} ".format(self.title))
         rl.console_set_default_foreground(0, oldColor) # Revert color before drawing rest of window.
-        for i,option in enumerate(self.options): # Draw the options.
+        for i,option in enumerate(self.options[self.optOffset:min(self.optOffset+self.optCap,len(self.options))]): # Draw the options.
             rl.console_print(0, self.x+4, self.y+1+i, option)
-        rl.console_print(0, self.x+2, self.y+1+self.selectedOption, chr(rl.CHAR_ARROW2_E))
+        rl.console_print(0, self.x+2, self.y+1+self.selectedOption-self.optOffset, chr(rl.CHAR_ARROW2_E))
+        # Draw scroll arrows as needed.
+        if int(time.clock()*2) % 2 == 0:
+            if self.optOffset > 0:
+                rl.console_print(0, self.x+int(self.w/2), self.y, chr(rl.CHAR_ARROW2_N))
+            if self.optOffset + self.optCap < len(self.options):
+                rl.console_print(0, self.x+int(self.w/2), self.y+self.h-1, chr(rl.CHAR_ARROW2_S))
     
     def forward(self): # If enter or the like is pressed and this is the active box.
         return self.options[self.selectedOption] # Return the selected item.
@@ -85,6 +95,14 @@ class SelectBox(Box): # This box allows selecting from a number of options.
     
     def goUp(self): # If up or the like is pressed.
         self.selectedOption = (self.selectedOption + len(self.options) - 1) % len(self.options) # Go up one option.
+        if self.selectedOption < self.optOffset: # If the option selected is above the listed options, fix that.
+            self.optOffset = self.selectedOption
+        if self.selectedOption + 1 >= self.optOffset + self.optCap: # If the option selected is below the listed options, fix that.
+            self.optOffset = self.selectedOption - self.optCap + 1
     
     def goDown(self): # If down or the like is pressed.
         self.selectedOption = (self.selectedOption + 1) % len(self.options) # Go down one option.
+        if self.selectedOption < self.optOffset: # If the option selected is above the listed options, fix that.
+            self.optOffset = self.selectedOption
+        if self.selectedOption + 1 >= self.optOffset + self.optCap: # If the option selected is below the listed options, fix that.
+            self.optOffset = self.selectedOption - self.optCap + 1
