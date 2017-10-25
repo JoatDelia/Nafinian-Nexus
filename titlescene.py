77391ecb83f251b926a9f6dd6f997046f3eb51b9
JoatDelia <Jesse_Hammond@cox.net>
2017-10-25 13:32:40 -0500

    Many methods of exit
    Selecting Exit on the main menu exits. Pressing Escape on the menu exits. Pressing alt+F4 exits. Sneezing does not exit.

--- titlescene.py
+++ titlescene.py
@@ -2,6 +2,9 @@ import libtcodpy as rl
 
 import boxes as bx
 
+import sys
+from libtcodpy import console_is_key_pressed
+
 class TitleScene: # As the name suggests, the title screen.
     def __init__(self):
         self.box = bx.SelectBox(33,17,14,6,None,("New Game","Continue","Modding","Exit")) # The main menu box.
@@ -15,8 +18,14 @@ class TitleScene: # As the name suggests, the title screen.
         if key.pressed == True: # Only process key press, not key release.
             if key.vk == rl.KEY_ENTER or key.vk == rl.KEY_SPACE or key.vk == rl.KEY_KPENTER:
                 command = self.box.forward() # Retrieve the selected option.
+                if command == "Exit":
+                    sys.exit() # Exit
                 print(command) # Just print it for now.
             elif key.vk == rl.KEY_DOWN or key.vk == rl.KEY_KP2:
                 self.box.goDown() # Go down one item.
             elif key.vk == rl.KEY_UP or key.vk == rl.KEY_KP8:
                 self.box.goUp() # Go up one item.
+            elif key.vk == rl.KEY_ESCAPE:
+                sys.exit() # Also exit
+            elif key.vk == rl.KEY_F4 and console_is_key_pressed(rl.KEY_ALT):
+                sys.exit() # Revenge of the exit
